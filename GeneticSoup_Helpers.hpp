b9e306a57bc2417aedae0b8f5b0db26be9ab1a57
makism <makhsm@gmail.com>
2011-05-23 20:39:32 +0300

    * Added utility class: singleton wrapper class to boost random.

---
+++ GeneticSoup_Helpers.hpp
@@ -0,0 +1,77 @@
+#ifndef _GENETICSOUP_HELPERS_HPP_
+#define _GENETICSOUP_HELPERS_HPP_
+
+
+#ifdef unix
+#   include <fstream>
+#else
+#   include <ctime>
+#endif
+#include "boost/random.hpp" 
+
+namespace GeneticSoup {
+    namespace Helpers {
+
+        // Based on
+        // http://www.mitchr.me/SS/exampleCode/boost/boostRandEx.cpp.html
+        // and http://sci.tuomastonteri.fi/programming/cplus/wrapper-to-boost-random
+        class Random {
+        public:
+            ~Random(void) {
+                delete uniDblGen;
+                delete uniDblUnit;
+                delete baseGen;
+            }
+            
+            static Random* Instance(void) {
+                if (mInstance==0)
+                    mInstance = new Random();
+                
+                return mInstance;
+            }
+            
+            double Generate(void) const {
+                return (*uniDblGen)();
+            }
+            
+        private:
+            Random(void) {
+                baseGen = new boost::minstd_rand(static_cast<unsigned int>(Seed()));
+                uniDblUnit = new boost::uniform_real<>(0, 1);
+                uniDblGen = new boost::variate_generator<boost::minstd_rand&, boost::uniform_real<> >(*baseGen, *uniDblUnit);
+            }
+            
+ #ifdef unix
+            uint64_t Seed() const
+            {
+                uint64_t seed;
+                std::ifstream urandom;
+                urandom.open("/dev/urandom");
+                urandom.read(reinterpret_cast<char*> (&seed), sizeof (seed));
+                urandom.close();
+                
+                return seed;
+            }
+ #else
+            uint64_t Seed() const;
+            {
+                return time(NULL);
+            }
+ #endif
+            
+            
+        private:
+            static Random* mInstance;
+                
+            boost::minstd_rand * baseGen;//(1234u);
+            boost::uniform_real<> * uniDblUnit;
+            boost::variate_generator<boost::minstd_rand&, boost::uniform_real<> > * uniDblGen; //(baseGen, uniDblUnit);
+        };
+        
+        Random* Random::mInstance = 0;
+    }
+}
+
+
+#endif
+ 
