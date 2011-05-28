f0136f4b695ca65ff338bff299f9139097baae5d
makism <makhsm@gmail.com>
2011-05-28 20:19:24 +0300

    * Updated example.

--- Examples/Population.cpp
+++ Examples/Population.cpp
@@ -15,19 +15,28 @@ int main(int argc, char** argv)
 
     srand((unsigned int) seconds);
     rand();
-    
+
     // Notice the pointer!
-    Population<StrGenome*> population(10, "initial");
+    Population<StrGenome*> population(20, "initial");
 
     for (unsigned int i=0; i<population.Size(); i++) {
-       StrGenome* g =  new StrGenome();
-       g->Create();
-       
-       population.Push(g);
+        StrGenome* g =  new StrGenome();
+        g->Create();
+        g->Evaluate();
+
+        population.Push(g);
     }
-    
+
     while (population.Next())
         std::cout << population.Current() << std::endl;
+    
+    std::cout << std::endl
+              << "Average Fitness: " << population.AverageFitness() << std::endl
+              << "   Best Fitness: " << population.BestFitness()    << std::endl
+              << "  Worst Fitness: " << population.WorstFitness()   << std::endl;
+              
+    std::cout << " Best genome: " << population.BestGenome() << std::endl
+              << "Worst genome: " << population.WorstGenome() << std::endl;
 
 #if _WIN32
     _CrtDumpMemoryLeaks();
