a18f2475135f3d812be77977d6566b317984087f
makism <makhsm@gmail.com>
2011-05-31 13:54:49 +0300

    * Code cleanups.

--- Examples/Pool.cpp
+++ Examples/Pool.cpp
@@ -1,7 +1,7 @@
 #include "GeneticSoup.hpp"
 #if _WIN32
-#	define _CRTDBG_MAP_ALLOC
-#	include <crtdbg.h>
+#   define _CRTDBG_MAP_ALLOC
+#   include <crtdbg.h>
 #endif
 
 typedef unsigned long long int uint64;
@@ -9,25 +9,26 @@ typedef unsigned long long int uint64;
 int main(int argc, char** argv) {
     GeneticSoup::Pool<uint64> pool(10);
 
-	pool.Push(1);		// push 1
-	pool.Push(2);		// push 2
-	pool.Assign(9, 10); // assign 10 as the last element of the pool
-	pool.Push(3);		// push 3
+    pool.Push(1);       // push 1
+    pool.Push(2);       // push 2
+    pool.Assign(9, 10); // assign 10 as the last element of the pool
+    pool.Push(3);       // push 3
 
-	uint64 &temp = pool.At(2);
-	temp = pool.First() + pool[9];
+    uint64 &temp = pool.At(2);
+    temp = pool.First() + pool[9];
 
-	if (!pool.IsEmpty()) {
-		while (pool.Next())	
-			std::cout << pool.Current() << std::endl;
-		pool.Reset();
-	}
-	
-	// or: std::cout << pool.ToString() << std::endl;
+    if (!pool.IsEmpty()) {
+        while (pool.Next())
+            std::cout << pool.Current() << std::endl;
+        pool.Reset();
+    }
+
+    // or: std::cout << pool.ToString() << std::endl;
 
 #if _WIN32
-	_CrtDumpMemoryLeaks();
+    _CrtDumpMemoryLeaks();
 #endif
     return 0;
 }
+
 
