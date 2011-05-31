b95d59ddd14fda7b7c637ad09011f36d7be91898
makism <makhsm@gmail.com>
2011-05-31 13:51:06 +0300

    * Code cleanups.

--- Headers/Selection.hpp
+++ Headers/Selection.hpp
@@ -9,8 +9,8 @@ class Selection {
 
 public:
     static T RouletteWheel(Population<T>&);
-//         static void Elitism(void);
-//         static void Tournament(void);
+    static void Elitism(Population<T>&, Population<T>&, unsigned int);
+//     static T Tournament(Population<T>&, unsigned int);
 //         static void BinaryTournament(void);
 //         static void PropBinaryTournament(void);
 };
--- Impl/SelectionImpl.hpp
+++ Impl/SelectionImpl.hpp
@@ -2,27 +2,36 @@
 
 namespace GeneticSoup {
 
-    template<class T>
-    T Selection<T>::RouletteWheel(Population<T>& pop) {
-        float slice = pop.TotalFitness() * Helpers::Random::Instance()->Generate();
-        float total = 0.0f;
-        
-        T selectedGenome = 0;
-        T currentGenome = 0;
-        
-        while (pop.Next()) {
-            currentGenome = pop.Current();
-            
-            total += currentGenome->Fitness();
-            
-            if (total > slice) {
-                selectedGenome = currentGenome;
-                break;
-            }
+template<class T>
+T Selection<T>::RouletteWheel(Population<T>& pop) {
+    float slice = pop.TotalFitness() * Helpers::Random::Instance()->Generate();
+    float total = 0.0f;
+
+    T selectedGenome = 0;
+    T currentGenome = 0;
+
+    while (pop.Next()) {
+        currentGenome = pop.Current();
+
+        total += currentGenome->Fitness();
+
+        if (total > slice) {
+            selectedGenome = currentGenome;
+            break;
         }
-        pop.Reset();
-        
-        return selectedGenome;
     }
+    pop.Reset();
+
+    return selectedGenome;
+}
+
+template<class T>
+void Selection<T>::Elitism(Population<T>& pop, Population<T>& newPop, unsigned int copies)
+{
+    std::sort(pop.Ref().begin(), pop.Ref().end());
+    
+//     for (unsigned int i=0; i<copies; i++)
+//         newPop.Push(t);
+}
 
 }
