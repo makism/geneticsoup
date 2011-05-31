b583ceeff0d62dd0f58e50d7ad332f21c28c0362
makism <makhsm@gmail.com>
2011-05-31 11:14:17 +0300

    * Implemented the evaluation method. It takes into consideration the number of the continuous one found, as well as the actual decimal number.

--- Examples/BinaryNumberGenome.hpp
+++ Examples/BinaryNumberGenome.hpp
@@ -5,7 +5,10 @@
 #include <algorithm>
 #include <time.h>
 #include <math.h>
-
+#include <iostream>
+#include <string>
+#include <bitset>
+#include <limits>
 #include "GeneticSoup.hpp"
 
 using namespace GeneticSoup;
@@ -23,8 +26,31 @@ public:
     
     float EvaluateCallback(void)
     {
-        return 0.0f;
+        float result = 0.0f;
+        int hits = 0;
+        bool continuous = true;
+        
+        std::stringstream ss;
+        for (int i=0; i<Size(); i++) {
+            ss << this->At(i);
+            
+            if (this->At(i)==1 && continuous)
+                hits++;
+            else
+                continuous = false;
+        }
+        
+        mNumber = std::bitset<std::numeric_limits<unsigned long>::digits>(ss.str()).to_ulong();
+        
+        result = hits * 0.2f;
+        result *= 0.5f;
+        result += (mNumber / 1023.0f) * 0.5f;
+        
+        return result;
     }
+    
+private:
+    unsigned int mNumber;
 };
 
 #endif
