96a5c3f07bf27981fa5670e462d43da15e61b50f
makism <makhsm@gmail.com>
2011-06-11 20:02:33 +0300

    * Updated example to reflect api changes (Elitism and RouletteWheel).

--- Examples/Selection.cpp
+++ Examples/Selection.cpp
@@ -8,6 +8,12 @@
 using namespace GeneticSoup;
 
 
+// struct compare {
+//     bool operator() (const StrGenome* g1, const StrGenome* g2) {
+//         return g1->Fitness() < g2->Fitness();
+//     }
+// };
+
 int main(int argc, char** argv)
 {
     time_t seconds;
@@ -18,6 +24,7 @@ int main(int argc, char** argv)
 
     // Notice the pointer!
     Population<StrGenome*> population(20, "initial");
+    Population<StrGenome*> nextPopulation(20, "2nd gen");
 
     for (unsigned int i=0; i<population.Size(); i++) {
         StrGenome* g =  new StrGenome();
@@ -26,14 +33,35 @@ int main(int argc, char** argv)
 
         population.Push(g);
     }
-    
+
+    Selection<StrGenome*>::Elitism(population, nextPopulation, 2);
     StrGenome* selected = Selection<StrGenome*>::RouletteWheel(population);
-    std::cout << selected << std::endl;
     
+    std::cout << "Current Population" << std::endl;
+    while (population.Next())
+        std::cout << population.Current() << std::endl;
+
+    std::cout << std::endl
+              << "Selected: " << selected
+              << std::endl;
+    
+    std::cout << std::endl << "Next Population" << std::endl;
+    for (unsigned int i=0; i<nextPopulation.Size() - 2; i++) {
+        StrGenome* g = new StrGenome();
+        g->Create();
+        g->Evaluate();
+        
+        nextPopulation.Push(g);
+    }
+    
+    while (nextPopulation.Next())
+        std::cout << nextPopulation.Current() << std::endl;
+
+
 #if _WIN32
     _CrtDumpMemoryLeaks();
 #endif
-    
+
     delete Helpers::Random::Instance();
 
     return 0;
